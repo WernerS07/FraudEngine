020ea858230b2482e5517cae0a7b1d559c0ab1a5
Werner Snyman <wsnyman@cougarnet.uh.edu>
2026-02-19 15:27:32 +0200

    Add files via upload

---
+++ Gateway/Program.cs
@@ -0,0 +1,40 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Load YARP config
+builder.Services.AddReverseProxy()
+    .LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"));
+// Keycloak settings
+var kc = builder.Configuration.GetSection("Keycloak");
+var authority = kc["Authority"]?.TrimEnd('/');
+var clientId = kc["Audience"];
+
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
+    {
+        options.Authority = $"{authority}";
+        options.Audience = clientId;
+        options.RequireHttpsMetadata = false;
+    });
+
+
+// Authorization policies
+builder.Services.AddAuthorization(options =>
+{
+    options.AddPolicy("BasicUser", p => p.RequireRole("basic-role", "audit-role"));
+    options.AddPolicy("Auditor", p => p.RequireRole("audit-role"));
+});
+
+var app = builder.Build();
+
+app.UseRouting();
+// Add auth checks to endpoints
+app.UseAuthentication();
+// Add role checks to endpoints
+app.UseAuthorization();
+
+// Map the proxy. mapping fraud and mockdata endpoints
+app.MapReverseProxy();
+
+app.Run();
